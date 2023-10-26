e49742670c90f1cf04ed4c587b3c7b7d43188833
冷醉泉 <2028983239@qq.com>
2023-10-26 19:07:49 +0800

    security

---
+++ qiniu-auth/src/main/java/com/qiniu/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.qiniu;
+
+/**
+ * Hello world!
+ *
+ */
+public class AuthApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
