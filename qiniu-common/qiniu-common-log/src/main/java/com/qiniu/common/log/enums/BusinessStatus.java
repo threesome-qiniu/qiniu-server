85d551d09edba00c1296c674927bcc62923b48f5
roydonGuo <3133010060@qq.com>
2023-10-26 20:56:08 +0800

    日志记录模块

---
+++ qiniu-common/qiniu-common-log/src/main/java/com/qiniu/common/log/enums/BusinessStatus.java
@@ -0,0 +1,16 @@
+package com.qiniu.common.log.enums;
+
+/**
+ * 操作状态
+ */
+public enum BusinessStatus {
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ qiniu-common/qiniu-common-log/src/main/java/com/qiniu/common/log/service/AsyncLogService.java
@@ -0,0 +1,11 @@
+package com.qiniu.common.log.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 异步调用日志服务
+ */
+@Service
+public class AsyncLogService {
+
+}
