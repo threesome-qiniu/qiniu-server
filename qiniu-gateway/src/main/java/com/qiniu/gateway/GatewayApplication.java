b5542428cb6e4072d42ad257cea53892c3016c48
roydonGuo <3133010060@qq.com>
2023-10-27 19:05:12 +0800

    搭建网关

--- qiniu-gateway/src/main/java/com/qiniu/gateway/GatewayApplication.java
+++ qiniu-gateway/src/main/java/com/qiniu/gateway/GatewayApplication.java
@@ -1,19 +1,19 @@
-package com.qiniu.gateway.user;
+package com.qiniu.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * UserGatewayApplication
+ * GatewayApplication
  *
  * @AUTHOR: roydon
  * @DATE: 2023/10/27
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserGatewayApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserGatewayApplication.class,args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
