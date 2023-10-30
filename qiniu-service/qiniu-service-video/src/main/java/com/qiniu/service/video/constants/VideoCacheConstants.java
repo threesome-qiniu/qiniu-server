ad44fd940e4b5e290a2736515badc2be58129ac4
冷醉泉 <2028983239@qq.com>
2023-10-30 14:29:22 +0800

    点赞，收藏，分类，建表

---
+++ qiniu-service/qiniu-service-video/src/main/java/com/qiniu/service/video/constants/VideoCacheConstants.java
@@ -0,0 +1,14 @@
+package com.qiniu.service.video.constants;
+
+/**
+ * UserCacheConstant
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/10/29
+ **/
+public class VideoCacheConstants {
+
+    public static final String VIDEO_INFO_PREFIX = "videoinfo:";
+
+    public static final long VIDEO_INFO_EXPIRE_TIME =  3600 * 24 ; //1天
+}
