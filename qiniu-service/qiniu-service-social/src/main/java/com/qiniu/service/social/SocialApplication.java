a8d64b2f99e854e5aec27e6790e0b737daddb8e6
roydonGuo <3133010060@qq.com>
2023-11-02 22:50:02 +0800

    依赖

--- qiniu-service/qiniu-service-social/src/main/java/com/qiniu/service/social/SocialApplication.java
+++ qiniu-service/qiniu-service-social/src/main/java/com/qiniu/service/social/SocialApplication.java
@@ -1,7 +1,15 @@
 package com.qiniu.service.social;
 
+import com.qiniu.common.annotations.EnableRedisConfig;
+import com.qiniu.common.annotations.EnableUserTokenInterceptor;
+import com.qiniu.common.config.MybatisPlusConfig;
+import com.qiniu.common.swagger.Swagger2Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * SocialApplication
@@ -10,6 +18,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @DATE: 2023/11/2
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.qiniu.feign")
+@EnableUserTokenInterceptor
+@EnableRedisConfig
+@EnableAsync
+@Import({MybatisPlusConfig.class, Swagger2Configuration.class})
 public class SocialApplication {
     public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
