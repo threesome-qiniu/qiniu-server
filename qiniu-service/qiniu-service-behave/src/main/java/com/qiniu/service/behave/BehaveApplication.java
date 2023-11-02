82d1b8218b89e085cd5589fd00bb6d5f022d75e5
roydonGuo <3133010060@qq.com>
2023-11-02 21:32:50 +0800

    视频行为服务

---
+++ qiniu-service/qiniu-service-behave/src/main/java/com/qiniu/service/behave/BehaveApplication.java
@@ -0,0 +1,17 @@
+package com.qiniu.service.behave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * BehaveApplication
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/11/2
+ **/
+@SpringBootApplication
+public class BehaveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BehaveApplication.class, args);
+    }
+}
