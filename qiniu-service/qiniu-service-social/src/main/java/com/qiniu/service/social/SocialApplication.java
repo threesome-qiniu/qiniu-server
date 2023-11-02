27dff1ee6bdad304ea5237b86f6cb330cdf97a57
roydonGuo <3133010060@qq.com>
2023-11-02 21:32:27 +0800

    用户社交服务

---
+++ qiniu-service/qiniu-service-social/src/main/java/com/qiniu/service/social/SocialApplication.java
@@ -0,0 +1,17 @@
+package com.qiniu.service.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SocialApplication
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/11/2
+ **/
+@SpringBootApplication
+public class SocialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+}
